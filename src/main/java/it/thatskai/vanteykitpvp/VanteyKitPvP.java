421ae1aabca907b3334751e52674d6af402cf4ed
ThatsKai <108898782+ThatsKai@users.noreply.github.com>
2023-05-12 15:29:15 +0200

    Add files via upload

---
+++ src/main/java/it/thatskai/vanteykitpvp/VanteyKitPvP.java
@@ -0,0 +1,17 @@
+package it.thatskai.vanteykitpvp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VanteyKitPvP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
